51bdd323bcecb2f4b15f696604eff7cc782c5c6b
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2026-08-30 15:40:10 +0400

    [AEGISORA-69388879] Injected guardian.

--- src/NumericRangeRuleGuardian.php
+++ src/NumericRangeRuleGuardian.php
@@ -2,6 +2,15 @@
 
 namespace Aegisora\RuleGuardians\NumericRangeRule;
 
+use Aegisora\Guardian\Guardian;
+
 class NumericRangeRuleGuardian
 {
+    private Guardian $guardian;
+
+    public function __construct(
+        Guardian $guardian
+    ) {
+        $this->guardian = $guardian;
+    }
 }
