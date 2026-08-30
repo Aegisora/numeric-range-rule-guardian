4f5b5870afa5c7970bf461124419c52d6640334e
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2026-08-30 15:43:22 +0400

    [AEGISORA-69388879] Implemented checkGreaterThan.

--- src/NumericRangeRuleGuardian.php
+++ src/NumericRangeRuleGuardian.php
@@ -2,7 +2,11 @@
 
 namespace Aegisora\RuleGuardians\NumericRangeRule;
 
+use Aegisora\Guardian\Exceptions\GuardianExecutingRuleException;
+use Aegisora\Guardian\Exceptions\GuardianValidationException;
 use Aegisora\Guardian\Guardian;
+use Aegisora\Rules\NumericRangeRule;
+use Throwable;
 
 class NumericRangeRuleGuardian
 {
@@ -12,5 +16,20 @@ class NumericRangeRuleGuardian
         Guardian $guardian
     ) {
         $this->guardian = $guardian;
+    }
+
+    /**
+     * @param mixed $value
+     * @param numeric $min
+     * @throws GuardianExecutingRuleException
+     * @throws GuardianValidationException
+     * @throws Throwable
+     */
+    public function checkGreaterThan(
+        $value,
+        $min,
+        ?Throwable $exception = null
+    ): void {
+        $this->guardian->check($value, NumericRangeRule::createGreaterThan($min), $exception);
     }
 }
